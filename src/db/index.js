166b962551e128623839062b43ea16894fdea6e6
Aashique-Ali <aashique.ali2002@gmail.com>
2024-05-04 16:24:17 +0500

    utils setup

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,19 @@
-require('dotenv').config({path:'./.env'})
-const mongoose = require("mongoose")
-const {DB_NAME} = require('../constants')
+require("dotenv").config({ path: "./.env" });
+const mongoose = require("mongoose");
+const { DB_NAME } = require("../constants");
 
+const ConnectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n Database connection succesfull!! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection error", error);
+    process.exit(1);
+  }
+};
 
-const ConnectDB = async () =>{
-    try {
-       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       console.log(`\n Database connection succesfull!! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MongoDB connection error", error);
-        process.exit(1)
-    }
-
-}
-
-module.exports= ConnectDB
+module.exports = ConnectDB;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-require('dotenv').config({path:'./env'})
-const ConnectDB = require('./db/index')
+require("dotenv").config({ path: "./env" });
+const { app } = require("./app");
+const ConnectDB = require("./db/index");
 
-
-
+const PORT = process.env.PORT;
 
 ConnectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection error", err);
+  });
